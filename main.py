45d06b9ce7de24c6413545cf1788b202e9b3e3cf
CezaryHenrykKos <>
2021-04-28 19:57:10 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,51 @@
+"""
+print("Day 1 - Python Print Function")
+print("The function is declared like this: ")
+print('print("What to print ?")')
+
+
+print(" qwerty \n qwerty \n qwerty")
+
+
+print("Hello Cezary")
+print("Hello " + "Cezary")
+print("Hello" + " " + "Cezary")
+
+
+#Fix the code below
+
+print("Day 1 - String Manipulation")
+print('String Concatention is done with the "+" sign.')
+print('e.g. print("Hello" + " World")')
+print("New lines can be created with a backshlash and n.")
+
+
+str = input("What is your name ? ")
+
+print("Lenght is :", len(str))
+
+print(len(input("What is your name ? ")))
+
+name = input("What is your name ? ")
+print(name)
+"""
+"""
+a = input("a: ")
+b = input("b: ")
+
+c = a
+a = b
+b = c
+
+print("a = " + a)
+print("b = " + b)
+
+"""
+
+#Final Project
+
+print("Welcome to the Band Name Generator")
+growupcity = input("What's of the city you grew up in ? \n")
+petname = input("What's your pet's name? \n")
+print("Your band name could be " + growupcity + " " + petname)
+
